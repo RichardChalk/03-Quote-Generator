e8653073583eae1735b22384573665ae4cbf2c25
Richard Chalk <barmybrit@gmail.com>
2025-02-03 11:51:40 +0100

    added arrow

--- FACIT/index.js
+++ FACIT/index.js
@@ -45,9 +45,18 @@ const quotes = [
   },
 ];
 
+// Normal
 btn.addEventListener("click", function () {
   let random = Math.floor(Math.random() * quotes.length);
 
   quote.innerText = quotes[random].quote;
   person.innerText = quotes[random].person;
 });
+
+// Arrow (mer modern!)
+// btn.addEventListener("click", () => {
+//   let random = Math.floor(Math.random() * quotes.length);
+
+//   quote.innerText = quotes[random].quote;
+//   person.innerText = quotes[random].person;
+// });
